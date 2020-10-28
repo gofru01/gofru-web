e40262beb0943590b47d4900b58fc082fe602130
Keivon <keivonproctor@hotmail.com>
2020-10-28 13:08:56 +0000

    Added dropdown menu

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,31 @@
-import Link from 'next/link'
+import Link from 'next/link';
+
 
 
 export default function Header() {
-    return(
-        <div>
-        <ul className='navbar'>
-          <li> <Link href="/"><a className="active" >Gofru</a></Link></li>
-          <ul style={{float:"right"}}>
+  return (
+    
+    <header class="header">
+      <ul>
+        <li> <Link href="/"><a>Gofru</a></Link></li>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+        <ul className="menu"  style={{ float: "right" }}>
           <li><Link href="/about"><a>About</a></Link></li>
+          <li><Link href="/blog"><a>Blog</a></Link></li>
+          <li className="dropdown">
+            <a href="#" className="dropbtn">Services</a>
+            <div className="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </li>
           <li><Link href="/contact"><a >Contact</a></Link></li>
-          </ul>
         </ul>
-      </div>
-    )
-  }
+      </ul>
+      </header>
+
+   
+  )
+}
---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+
+
+export default function About() {
+    return (
+        <main style={{height:"100vw", textAlign: "center"}}>
+            <h1 className>
+                Welcome to Blog
+        </h1>
+        </main>
+    )
+}
